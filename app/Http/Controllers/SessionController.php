642a9ffd7f68dbbba6a71e3e568bbaac0b9ee860
Sascha <S.holzigel@gmx.de>
2024-10-19 11:48:31 +0200

    Updated UserController for register, login and logut

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller {
+    public function create() {
+
+        return view('auth.login');
+    }
+
+    public function store(Request $request) {
+        $attributes = request()->validate([
+            'email'    => ['required', 'email', 'max:254'],
+            'password' => ['required'],
+        ]);
+
+        if( !Auth::attempt($attributes)){
+            throw ValidationException::withMessages([
+                'password' => __('app.incorrect_password'),
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/customers');
+    }
+
+    public function destroy() {
+        Auth::logout();
+
+        return redirect('/login');
+    }
+}
