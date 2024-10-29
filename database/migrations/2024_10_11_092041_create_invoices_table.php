110a5b993b70323e44c4ef2bcde1a83052440506
Sascha <S.holzigel@gmx.de>
2024-10-29 21:14:38 +0100

    create migration for reports

--- database/migrations/2024_10_11_092041_create_invoices_table.php
+++ database/migrations/2024_10_11_092041_create_invoices_table.php
@@ -4,14 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('invoices', function (Blueprint $table) {
+    public function up(): void {
+        Schema::create('reports', function(Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -20,8 +18,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('invoices');
+    public function down(): void {
+        Schema::dropIfExists('reports');
     }
 };
